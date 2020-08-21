204114164c0b56f8cc2d32cbed2538c926e933f7
Festus Ebin <ebinfestus@gmail.com>
2020-08-21 15:24:10 +0100

    fixed issues

--- src/App.test.js
+++ src/App.test.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 //import { render } from '@testing-library/react';
 import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
