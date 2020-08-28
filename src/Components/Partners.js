cd888e89b50a6b21587b6881b68a1851e298fc54
Alade <alade.faruk11@gmail.com>
2020-08-28 12:11:50 +0100

    Worked on main content

---
+++ src/Components/Partners.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Partner.css"
+
+
+const Partners =()=> {
+  return (
+   <div className="Pcover">
+       <div className="Pimg"></div>
+       <div className="Pimg"></div>
+       <div className="Pimg"></div>
+       <div className="Pimg"></div>
+       <div className="Pimg"></div>
+       <div className="Pimg"></div>
+       <div className="Pimg"></div>
+
+  
+   </div>
+ 
+  
+  );
+}
+
+export default Partners;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import MainContent from "./Containers/MainContent";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.render(<MainContent/> ,
   document.getElementById("root")
 );
